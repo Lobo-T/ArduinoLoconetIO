0e1d4414eb89482a41c782a41ab0b28de230fc33
Lobo-T <trondmh+git@gmail.com>
2016-10-03 21:06:41 +0200

    Litt fler kommentarer.

--- LoconetIO.ino
+++ LoconetIO.ino
@@ -2,12 +2,16 @@
 
 //#define DEBUG
 
-//Locoshield transmit pin.
+//Locoshield transmit pin. (Receive pin må alltid være 8 (ICP1) på UNO, og 48 (ICP5) på MEGA).
 #define LNtxPin 7
+
 //Startadresse.  JMRI Hardware Address. Verdi mellom 1 og 2048.  Unngå 1017 - 1020.
+//Dette er adressen til første inngang og utgang.  De andre i arrayen blir nummerert fortløpende.
+//Eksempel:  Med JMRI_ADR 1, fire utganger og 8 innganger vil du få JMRI adresser LL1 til LL4 på utgangene
+//og LS1 til LS8 på inngangene.
 #define JMRI_ADR 1
 
-//Setter navn på pinnenummre
+//Setter navn på pinnenummre.
 #define ut0 3
 #define ut1 4
 #define ut2 5
@@ -20,7 +24,6 @@
 #define inn5 A1
 #define inn6 A2
 #define inn7 A3
-
 
 
 //De pinnene som er i denne arrayen vil bli satt som innganger
@@ -59,6 +62,8 @@ void setup() {
   for (byte i = 0; i < sizeof(utportPins); i++) {
     pinMode(utportPins[i], OUTPUT);
   }
+  //Over blir alle pinnene i innportPins satt til input med innebyggde pullupmotstander slått på.
+  //Hvis du har behov for at noen av dem ikke har pullup så bare redefiner dem det gjelder som INPUT under her.
 
   //Vi leser pinnene og oppdaterer innportStateLast før vi starter hovedprogrammet.
   for (byte i = 0; i < sizeof(innportPins); i++) {
@@ -154,7 +159,7 @@ void notifySwitchRequest( uint16_t Address, uint8_t Output, uint8_t Direction ) 
 
   //Sensor forespørsel fra JMRI, rapporter status på innganger.
   //Hvilken av forespørslene (1017-1020) vi egentlig skal svare på er avhengig av hvilken adresse vi har
-  //men jeg orker ikke prøve å finne ut av hvordan dettte egentlig er ment å fungrere nå, så vi svarer på 1018.
+  //men jeg orker ikke prøve å finne ut av hvordan dettte egentlig er ment å fungere nå, så vi svarer på 1018.
   if (!Output && !Direction && Address == 1018) {
     for (int i = 0; i < sizeof(innportPins); i++) {
       LN_STATUS lnstat = LocoNet.reportSensor(i + JMRI_ADR, innportState[i]);
